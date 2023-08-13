3f6e2c93bb9e37c54e179417b7d3471e7c5b9e7c
limbospoon <magicspoon78@gmail.com>
2023-08-13 16:23:03 +0100

    added seconds list and made list

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import ListGroup from "./components/ListGroup";
 
-function App()
-{
-  return <div><ListGroup /></div>
+function App() {
+  const weapons = ["Sword", "Mace", "Staff", "Spear"];
+  const armour = ["Chitin", "Iron", "Dragon Bone", "Ebony"];
+  return (
+    <div>
+      <ListGroup items={weapons} heading="Weapons"/>
+      <ListGroup items={armour} heading="Armour"/>
+    </div>
+  );
 }
 
 export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
 
-function ListGroup() {
-  const items = ["Sword", "Mace", "Staff", "Spear"];
+interface ListGroupProps {
+  items: string[];
+  heading: string;
+}
 
+function ListGroup(props: ListGroupProps) {
+  
   /* Make react dynamic var for compent */
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   /* Check if list is empty */
-  const ListSizeCheck = items.length === 0 && <p>No Item Found</p>;
+  const ListSizeCheck = props.items.length === 0 && <p>No Item Found</p>;
   /* Function that returns ListSizeCheck variable */
   const ListIsEmpty = () => {
     return ListSizeCheck;
@@ -16,15 +20,25 @@ function ListGroup() {
   return (
     /* Can only return html elements but using curly braces allows you to return more then html elements */
     <>
-      <h1>List</h1>
+      <h1>{props.heading}</h1>
       {/* Call ListIsEmpty function this allows the page to change dynamic depending
         if the list is empty or not */}
       {ListIsEmpty()}
       {/* class needs to be changed to className for JSX */}
       <ul className="list-group">
         {/* Covert items to list elements. Note: must be done between curly braces */}
-        {items.map((item, index) => (
-          <li key={item} className={selectedIndex === index ? "list-group-item active" : "list-group-item"} onClick={() => {setSelectedIndex(index);}}>
+        {props.items.map((item, index) => (
+          <li
+            key={item}
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
             {item}
           </li>
         ))}
