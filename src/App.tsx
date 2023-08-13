2104317c3d6219ec92ce52f6bcd4545c47a93eae
limbospoon <magicspoon78@gmail.com>
2023-08-13 16:41:06 +0100

    added selectitem callback function

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,15 @@ import ListGroup from "./components/ListGroup";
 function App() {
   const weapons = ["Sword", "Mace", "Staff", "Spear"];
   const armour = ["Chitin", "Iron", "Dragon Bone", "Ebony"];
+
+  const handleOnItemSelect = (item: string) => {
+    console.log("Selected ", item)
+  }
+
   return (
     <div>
-      <ListGroup items={weapons} heading="Weapons"/>
-      <ListGroup items={armour} heading="Armour"/>
+      <ListGroup items={weapons} heading="Weapons" onSelectItem={handleOnItemSelect}/>
+      <ListGroup items={armour} heading="Armour" onSelectItem={handleOnItemSelect}/>
     </div>
   );
 }
